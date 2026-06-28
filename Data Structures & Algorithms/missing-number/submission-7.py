4b755e33746494663f7cc8f11159cf1b0fdbc151
ADITYA MISHRA <admrs157@gmail.com>
2026-06-28 23:31:56 +0530

    Add: missing-number - submission-7

---
+++ Data Structures & Algorithms/missing-number/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        upper_limit = len(nums)
+        # for i in range(upper_limit + 1):
+        #     if i not in nums:
+        #         return i
+        # return 0
+        real_sum = int(upper_limit * (upper_limit + 1 ) / 2 )
+        actual_sum = sum(nums)
+        return real_sum - actual_sum
+    

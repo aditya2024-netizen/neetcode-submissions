0653f7a226e336518b3bcfb25a285d09c8175448
ADITYA MISHRA <admrs157@gmail.com>
2026-06-30 23:10:48 +0530

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # optimal
+        absolute = abs(x)
+        rev  = 0
+        if x < 0 :
+            sign = -1
+        else:
+            sign = 1
+        while absolute!=0:
+            digit = absolute%10
+            rev = rev*10 + digit
+            absolute//=10
+        rev*=sign
+        if rev < -(2**31) or rev > 2**31 - 1:
+            return 0
+        return rev
